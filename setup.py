952cedcc548392aa042b627fe2a2336a969a96df
Josh Feldman <joshfeldman95@gmail.com>
2019-05-06 17:42:22 -0400

    imported os

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup, find_packages
 from setuptools.command.install import install
 import re
+import os
 
 class CustomInstallCommand(install):
     """Customized setuptools install command - prints a friendly greeting."""
